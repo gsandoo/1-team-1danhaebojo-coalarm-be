c75a93bfe459e39bd778e605e7ee18425fc658b2
hyoon1129 <hyoon1129@naver.com>
2025-03-14 20:08:27 +0900

    feat: MACD 계산 API 추가
    - 특정 코인의 MACD 값을 계산하는 API 추가
    - 향후 RSI 등 지표 추가하여 대시보드용 통합 API로 확장 예정

--- src/main/java/_1danhebojo/coalarm/coalarm_service/domain/dashboard/controller/DashboardController.java
+++ src/main/java/_1danhebojo/coalarm/coalarm_service/domain/dashboard/controller/DashboardController.java
@@ -1,4 +1,24 @@
 package _1danhebojo.coalarm.coalarm_service.domain.dashboard.controller;
 
+import _1danhebojo.coalarm.coalarm_service.domain.dashboard.controller.response.MacdDTO;
+import _1danhebojo.coalarm.coalarm_service.domain.dashboard.service.CoinMarketService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/dashboard")
+@RequiredArgsConstructor
 public class DashboardController {
+    private final CoinMarketService coinMarketService;
+
+    @GetMapping("/{symbol}/macd")
+    public ResponseEntity<MacdDTO> getMacd(@PathVariable("symbol") String symbol) {
+        MacdDTO response = coinMarketService.getMacdForSymbol(symbol);
+        return ResponseEntity.ok(response);
+    }
+
 }
