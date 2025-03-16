e323c432f0ff5a8ffabedee5a7551b13bcdbb0fa
Dwisgolmog <rlaehdgus134@naver.com>
2025-03-16 22:49:25 +0900

    feat: 김치 프리미엄 계산후 DB저장
    스케쥴러를 통해 5분에 한번씩 ticker 데이터를 가져와서 계산후 DB에 데이터를 저장하는 로직을 추가하였음(초안이기에 추후에 변동 발생할 수 있음)

--- src/main/java/_1danhebojo/coalarm/coalarm_service/CoalarmServiceApplication.java
+++ src/main/java/_1danhebojo/coalarm/coalarm_service/CoalarmServiceApplication.java
@@ -2,7 +2,9 @@ package _1danhebojo.coalarm.coalarm_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class CoalarmServiceApplication {
 
--- src/main/java/_1danhebojo/coalarm/coalarm_service/domain/dashboard/repository/KimchiPremiumRepository.java
+++ src/main/java/_1danhebojo/coalarm/coalarm_service/domain/dashboard/repository/KimchiPremiumRepository.java
@@ -1,10 +1,19 @@
 package _1danhebojo.coalarm.coalarm_service.domain.dashboard.repository;
 
+import _1danhebojo.coalarm.coalarm_service.domain.dashboard.repository.entity.CoinEntity;
 import _1danhebojo.coalarm.coalarm_service.domain.dashboard.repository.entity.KimchiPremiumEntity;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 public interface KimchiPremiumRepository {
     List<KimchiPremiumEntity> findAllKimchiPremiums(int offset,int limit);
     void saveKimchiPremium(KimchiPremiumEntity kimchiPremiumEntity);
+
+    Optional<KimchiPremiumEntity> findTopByCoinAndRegDtBetweenOrderByRegDtDesc(
+            CoinEntity coin,
+            LocalDateTime fromDateTime,
+            LocalDateTime toDateTime
+    );
 }
