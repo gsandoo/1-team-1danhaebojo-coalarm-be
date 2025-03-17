e6718f21cfca8db34e2f798891b61b459867d86c
hyoon1129 <hyoon1129@naver.com>
2025-03-17 15:46:22 +0900

    feat: 유저 서비스 및 회원가입 기능 추가
    - UserService 및 UserServiceImpl 추가
    - UserEntity 및 관련 리포지토리 추가
    - UserDTO 추가하여 유저 응답 데이터 매핑

---
+++ src/main/java/_1danhebojo/coalarm/coalarm_service/domain/user/service/UserServiceImpl.java
@@ -0,0 +1,39 @@
+package _1danhebojo.coalarm.coalarm_service.domain.user.service;
+
+import _1danhebojo.coalarm.coalarm_service.domain.user.controller.response.UserDTO;
+import _1danhebojo.coalarm.coalarm_service.domain.user.entity.UserEntity;
+import _1danhebojo.coalarm.coalarm_service.domain.user.repository.UserRepository;
+import _1danhebojo.coalarm.coalarm_service.domain.user.util.NicknameGenerator;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDTO registerOrLogin(String kakaoId, String email) {
+        Optional<UserEntity> userOptional = userRepository.findByKakaoId(kakaoId);
+
+        if (userOptional.isPresent()) {
+            return UserDTO.fromEntity(userOptional.get());
+        }
+
+        String randomNickname = NicknameGenerator.generateNickname(); // 랜덤 닉네임 생성
+        UserEntity newUser = UserEntity.builder()
+                .kakaoId(kakaoId)
+                .nickname(randomNickname)
+                .email(email)
+                .profileImg(null)
+                .build();
+
+        UserEntity savedUser = userRepository.save(newUser);
+        return UserDTO.fromEntity(savedUser);
+    }
+}
