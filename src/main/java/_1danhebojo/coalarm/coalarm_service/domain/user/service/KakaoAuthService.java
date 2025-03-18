1d8911f5055edf5d5ebd5a378eba6d17014ee91a
DongHyun Kim <95118426+Dwisgolmog@users.noreply.github.com>
2025-03-18 09:03:13 +0900

    Revert "카카오 OAuth 기반 로그인 기능 추가"

--- src/main/java/_1danhebojo/coalarm/coalarm_service/domain/user/service/KakaoAuthService.java
+++
@@ -1,8 +0,0 @@
-package _1danhebojo.coalarm.coalarm_service.domain.user.service;
-
-import java.util.Map;
-
-public interface KakaoAuthService {
-    String getAccessToken(String code);
-    Map<String, String> getUserInfo(String accessToken);
-}
