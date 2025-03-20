98aad7991714bdbf0ed2308abede66521173b59e
jeonggu <dev.jeonggu@gmail.com>
2025-03-20 16:21:22 +0900

    feat: 스케줄링 설정 추가
    - Spring Scheduler를 사용하여 특정 작업을 일정 간격으로 실행하도록 설정

--- src/main/java/_1danhebojo/coalarm/coalarm_service/CoalarmServiceApplication.java
+++ src/main/java/_1danhebojo/coalarm/coalarm_service/CoalarmServiceApplication.java
@@ -2,8 +2,10 @@ package _1danhebojo.coalarm.coalarm_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CoalarmServiceApplication {
 
 	public static void main(String[] args) {
