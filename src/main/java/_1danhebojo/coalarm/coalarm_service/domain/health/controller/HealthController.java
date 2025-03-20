070f89a796e00fa1d9788299dea1e94f2c57b6c6
sakang <rogersddt@naver.com>
2025-03-20 14:57:05 +0900

    feat: health check controller 테스트

--- src/main/java/_1danhebojo/coalarm/coalarm_service/domain/health/controller/HealthController.java
+++ src/main/java/_1danhebojo/coalarm/coalarm_service/domain/health/controller/HealthController.java
@@ -1,9 +1,15 @@
 package _1danhebojo.coalarm.coalarm_service.domain.health.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/")
+@RequiredArgsConstructor
 public class HealthController {
-    @GetMapping("/")
+    @GetMapping("")
     public String health() {
         return "health check successsful";
     }
