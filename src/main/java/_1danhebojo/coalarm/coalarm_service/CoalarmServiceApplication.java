f6557bef521c4351c4038b74c520f970916bdb74
hyoon1129 <hyoon1129@naver.com>
2025-03-24 21:06:58 +0900

    refactor: 불필요한 설정 제거 및 updateDiscordWebhook 조건문 추가

--- src/main/java/_1danhebojo/coalarm/coalarm_service/CoalarmServiceApplication.java
+++ src/main/java/_1danhebojo/coalarm/coalarm_service/CoalarmServiceApplication.java
@@ -6,10 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @EnableScheduling
-@EnableWebSecurity(debug = true)
 @SpringBootApplication
 @EnableConfigurationProperties({KakaoProperties.class, JwtProperties.class})
 public class CoalarmServiceApplication {
