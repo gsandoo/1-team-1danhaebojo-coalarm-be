808f8b73595e3dc2f80453f9833a2e560ec273c1
jeonggu <dev.jeonggu@gmail.com>
2025-03-26 22:23:04 +0900

    refactor: exception 관련 수정
    - ApiException으로 통일

--- src/main/java/_1danhebojo/coalarm/coalarm_service/domain/alert/service/AlertService.java
+++ src/main/java/_1danhebojo/coalarm/coalarm_service/domain/alert/service/AlertService.java
@@ -4,6 +4,8 @@ import _1danhebojo.coalarm.coalarm_service.domain.alert.controller.request.*;
 import _1danhebojo.coalarm.coalarm_service.domain.alert.controller.response.AlertResponse;
 import _1danhebojo.coalarm.coalarm_service.domain.alert.repository.AlertRepository;
 import _1danhebojo.coalarm.coalarm_service.domain.alert.repository.entity.*;
+import _1danhebojo.coalarm.coalarm_service.global.api.ApiException;
+import _1danhebojo.coalarm.coalarm_service.global.api.AppHttpStatus;
 import _1danhebojo.coalarm.coalarm_service.global.api.OffsetResponse;
 import _1danhebojo.coalarm.coalarm_service.domain.user.repository.entity.UserEntity;
 import lombok.RequiredArgsConstructor;
@@ -31,77 +33,31 @@ public class AlertService {
         Alert alert = convertToAlertEntity(request);
 
         // 해당 알람의 코인을 등록한 적이 있는지 체크
-//        if(alertSSEService.isAlertSetForSymbolAndType(request.getUserId(), request.getSymbol(), request.getType()))
-//        {
-//            throw new RuntimeException("🚨 이미 등록된 알람 있음!");
-//        }
+        if(alertSSEService.isAlertSetForSymbolAndType(request.getUserId(), request.getSymbol(), request.getType()))
+        {
+            throw new ApiException(AppHttpStatus.ALREADY_EXISTS_ALERT);
+        }
 
         Alert savedAlert = alertRepository.save(alert);
 
         Optional<Alert> checkAlert = alertRepository.findById(alert.getAlertId());
         if (checkAlert.isEmpty()) {
-            throw new RuntimeException("🚨 flush() 후에도 저장 안 됨!");
+
+            throw new ApiException(AppHttpStatus.FAILED_TO_SAVE_ALERT);
         }
 
         Long alertId = savedAlert.getAlertId();
         if (alertId == null) {
-            throw new RuntimeException("Alert 저장 실패");
+            throw new ApiException(AppHttpStatus.FAILED_TO_SAVE_ALERT);
         }
 
-        switch (request.getType()) {
-            case "TARGET_PRICE":
-                TargetPriceAlert targetPriceAlert = new TargetPriceAlert();
-                targetPriceAlert.setPrice(((TargetPriceAlertRequest) request).getPrice());
-                targetPriceAlert.setPercentage(((TargetPriceAlertRequest) request).getPercentage());
-
-                Alert tartgetAlert = new Alert();
-                tartgetAlert.setAlertId(alertId);
-                targetPriceAlert.setAlert(tartgetAlert);
-
-                savedAlert.setTargetPrice(targetPriceAlert);
-                Long target = alertRepository.saveTargetPriceAlert(targetPriceAlert);
-                if (target == null) {
-                    throw new RuntimeException("Target Price Alert 저장 실패");
-                }
-                break;
-
-            case "GOLDEN_CROSS":
-                GoldenCrossAlert goldenCrossAlert = new GoldenCrossAlert();
-
-                Alert goldenAlert = new Alert();
-                goldenAlert.setAlertId(alertId);
-                goldenCrossAlert.setAlert(goldenAlert);
-
-                Long goldenCrossId = alertRepository.saveGoldenCrossAlert(goldenCrossAlert);
-                if (goldenCrossId == null) {
-                    throw new RuntimeException("Golden Cross Alert 저장 실패");
-                }
-                break;
-
-            case "VOLUME_SPIKE":
-                VolumeSpikeAlert volumeSpikeAlert = new VolumeSpikeAlert();
-                volumeSpikeAlert.setTradingVolumeSoaring(((VolumeSpikeAlertRequest) request).getTradingVolumeSoaring());
-
-                Alert volumeAlert = new Alert();
-                volumeAlert.setAlertId(alertId);
-                volumeSpikeAlert.setAlert(volumeAlert);
-
-                Long volumeSpikeId = alertRepository.saveVolumeSpikeAlert(volumeSpikeAlert);
-                if (volumeSpikeId == null) {
-                    throw new RuntimeException("Volume Spike Alert 저장 실패");
-                }
-                break;
-
-            default:
-                throw new IllegalArgumentException("잘못된 알람 타입: " + request.getType());
-        }
         alertSSEService.addEmitter(request.getUserId(), checkAlert.get());
     }
 
     // 알람 활성화 수정
     public Long updateAlertStatus(Long alertId, boolean active) {
         Alert alert = alertRepository.findById(alertId)
-                .orElseThrow(() -> new RuntimeException("Alert not found"));
+                .orElseThrow(() -> new ApiException(AppHttpStatus.NOT_FOUND_ALERT));
         boolean isActive = alert.isActive();
 
         if(active != isActive) {
@@ -120,7 +76,7 @@ public class AlertService {
     // 알람 삭제
     public void deleteAlert(Long alertId) {
         Alert alert = alertRepository.findById(alertId)
-                .orElseThrow(() -> new RuntimeException("Alert not found"));
+                .orElseThrow(() -> new ApiException(AppHttpStatus.NOT_FOUND_ALERT));
 
         alertSSEService.deleteEmitter(alert.getUser().getUserId(), alert);
         alertRepository.deleteById(alertId);
@@ -184,7 +140,7 @@ public class AlertService {
         // 우선적으로 추가 추후에 변경 필요
         if (request.getSymbol() != null) {
             Coin coin = alertRepository.findCoinBySymbol(request.getSymbol())
-                    .orElseThrow(() -> new RuntimeException("해당 심볼의 코인이 존재하지 않습니다."));
+                    .orElseThrow(() -> new ApiException(AppHttpStatus.NOT_FOUND_COIN));
             alert.setCoin(coin);
         }
 
