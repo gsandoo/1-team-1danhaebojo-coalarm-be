edc70c43f12830980974ad935476191e51e35123
jeonggu <dev.jeonggu@gmail.com>
2025-03-28 01:50:25 +0900

    refactor: SSE 알람 response 추가
    - SSE 알람 response 추가

---
+++ src/main/java/_1danhebojo/coalarm/coalarm_service/domain/alert/controller/response/AlertSSEResponse.java
@@ -0,0 +1,51 @@
+package _1danhebojo.coalarm.coalarm_service.domain.alert.controller.response;
+
+import _1danhebojo.coalarm.coalarm_service.domain.alert.repository.entity.Alert;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class AlertSSEResponse {
+    private Long alertId;
+    private String title;
+    private String coinName;
+    private String nickname;
+
+    private boolean targetPriceFlag;
+    private boolean goldenCrossFlag;
+    private boolean volumeSpikeFlag;
+
+    private TargetPriceDetail targetPrice;
+
+    public AlertSSEResponse(Alert alert) {
+        this.alertId = alert.getAlertId();
+        this.title = alert.getTitle();
+        this.coinName = alert.getCoin().getName();
+        this.nickname = alert.getUser().getNickname();
+
+        this.targetPriceFlag = alert.isTargetPriceFlag();
+        this.goldenCrossFlag = alert.isGoldenCrossFlag();
+        this.volumeSpikeFlag = alert.isVolumeSpikeFlag();
+
+        if (alert.isTargetPriceFlag() && alert.getTargetPrice() != null) {
+            this.targetPrice = new TargetPriceDetail(
+                    alert.getTargetPrice().getPrice(),
+                    alert.getTargetPrice().getPercentage()
+            );
+        }
+    }
+
+    public static class TargetPriceDetail {
+        private double price;
+        private double percentage;
+
+        public TargetPriceDetail(double price, double percentage) {
+            this.price = price;
+            this.percentage = percentage;
+        }
+
+        public double getPrice() { return price; }
+        public double getPercentage() { return percentage; }
+    }
+}
