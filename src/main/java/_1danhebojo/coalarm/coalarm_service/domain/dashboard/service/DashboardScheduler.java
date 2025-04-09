3795b5d8201dec273eca0807772f7cf1c9ee0bfc
Dwisgolmog <rlaehdgus134@naver.com>
2025-04-09 23:16:11 +0900

    refactor: 스케쥴러 분리
    - 김치 프리미엄과 코인 지표의 스케쥴러주기를 다르게 하기 위하여 분리하였음
    - 김치 프리미엄의 로그 주석처리

--- src/main/java/_1danhebojo/coalarm/coalarm_service/domain/dashboard/service/DashboardScheduler.java
+++ src/main/java/_1danhebojo/coalarm/coalarm_service/domain/dashboard/service/DashboardScheduler.java
@@ -1,9 +1,12 @@
 package _1danhebojo.coalarm.coalarm_service.domain.dashboard.service;
 
+import _1danhebojo.coalarm.coalarm_service.domain.dashboard.controller.response.ResponseKimchiPremium;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -12,11 +15,30 @@ public class DashboardScheduler {
     private final KimchiPremiumService kimchiPremiumService;
     private final CoinIndicatorService coinIndicatorService;
 
-    @Scheduled(fixedRate = 3000000)
-    public void calculateAndSaveKimchiPremiumTask(){
-        log.info("대시보드 데이터 계산 시작...");
-        kimchiPremiumService.calculateAndSaveKimchiPremium();
+    // TODO: 멀티 스레드를 통해 김치 프리미엄 계산시간을 다른 곳에 영향이 가지 않게하기
+
+    @Scheduled(fixedRate = 60000)
+    public void calculateCoinIndicators(){
+        log.info("코인지표 데이터 계산 시작...");
+        long startTime = System.currentTimeMillis();
+
         coinIndicatorService.saveIndicators("BTC");
-        log.info("대시보드 데이터 계산 완료.");
+
+        long endTime = System.currentTimeMillis();
+        long executionTime = endTime - startTime;
+        log.info("코인지표 데이터 계산 완료. 실행 시간: " + executionTime + "ms");
+    }
+
+    @Scheduled(fixedRate = 300000)
+    public void calculateAndSaveKimchiPremiumTask(){
+
+        log.info("김치프리미엄 데이터 계산 시작...");
+        long startTime = System.currentTimeMillis();
+
+        kimchiPremiumService.calculateAndSaveKimchiPremium();
+
+        long endTime = System.currentTimeMillis();
+        long executionTime = endTime - startTime;
+        log.info("김치프리미엄 데이터 계산 완료. 실행 시간: " + executionTime + "ms");
     }
 }
